21e02f40265e2527c314c17aab037853d8acb34f
Muhammad Muzammil Umar <muzammil.coredigitals@gmail.com>
2025-04-24 04:59:37 +0500

    terms-update

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -12,42 +12,75 @@ include('includes/header.php');
                 <h1 class="text-center"><strong>Terms & Conditions</strong></h1>
                 <div class="vspace-2em"></div>
                 <div class="vspace-2em"></div>
-                <h3>Copyright</h3>
-                <p>All of the content you see on this web site, including all page headers,
-                    images, graphics and text are subject to trademark, copyright and or other
-                    intellectual property rights or licenses held by <?php echo WEBSITE_NAME; ?>. The entire
-                    content of this web site is copyrighted as a collective work under United
-                    States copyright laws. <?php echo WEBSITE_NAME; ?>. owns a copyright in the selection,
-                    coordination, arrangement and enhancement of the content.</p>
-                <p>You may copy, download, reproduce, modify, publish, distribute, transmit,
-                    transfer or create works from the content under the conditions that (1) the
-                    republication is not for resale or any type of commercial use, (2) the name
-                    of this company/web site, <?php echo WEBSITE_NAME; ?>., is prominently displayed.</p>
-                <p>By accessing and using <?php echo WEBSITE_NAME; ?>. you agree that your access to
-                    and use of this web site is subject to these terms and conditions, as well as
-                    all applicable laws, as governed and interpreted pursuant to the state of
-                    Washington, and United States of America.</p>
+                <p>Your use of this Website and/or our services constitutes Your agreement to the following terms and conditions. If You do not agree with any of these terms and conditions, please do not use this Website or Our services.
+                    If You are under legal age of consent for Your respective district, You are not allowed to access or use this Website or Our services. You acknowledge and agree that You must be of legal age to purchase any of Our products or services available on this Website or otherwise.
+                    By submitting an order and/or payment, You are acknowledging that You have read and understand these terms and conditions. By submitting an order and/or payment, You are also agreeing to be legally bound by these terms and conditions, which form the entire agreement between You and <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a>.</p>
+                <h3>Definition of Terms
+                </h3>
+                <p>“Website” means all online content on <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a> website pages.
+                    “Customer,” “You,” or “Yours” mean and refer to you and/or any other person submitting an order to <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a> on your behalf.
+                    “Company,” “We,” or “Our” mean and refer to <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a>, a company registered under the laws of Massachusetts.
 
-                <h3>Right of Refusal</h3>
-                <p><?php echo WEBSITE_NAME; ?>. reserves the right to not print any book that may not
-                    comply with copyright or intellectual property laws or that we decide is
-                    pornographic or unsuitable for any reason.</p>
+                    “Product” means and refers to all services and products provided by <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a> to the Customer in accordance with his/her Order.
+                    “Order” means an order via phone or e-mail made by the Customer to purchase services or products provided by <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a>. Orders are confirmed by down payments via credit card, debit card, bank wire transfer, Western Union, or PayPal transfer.</p>
+                <h3>Our Services
+                </h3>
+                <p>By submitting the order and/or payment, you are acquiring the product solely for your personal use. Refunds for all services are available in the event of dissatisfaction with the quality, and warranties can be adjusted or revised based on feedback. It is your responsibility to thoroughly read and understand these terms and conditions before submitting any order or payment through this website.
 
-                <h3>Check Return Charge</h3>
-                <p>Any check or payment issued to <?php echo WEBSITE_NAME; ?>. for the purchase of
-                    products or services and returned by a financial institution to Gorham
-                    Printing for any reason including but not limited to insufficient funds,
-                    will be accessed a fee of $50.00 per check or item issued. This fee must
-                    be paid before further products or services can be ordered.</p>
+                </p>
+                <h3>
+                    REFUND POLICY
+                </h3>
+                <p>Refunds must be claimed within 120 days of delivery. Refunds claimed after the set time frame will not be entertained.
+                </p>
+                <p>All customers must note the deadline for claiming a refund at the time of placing an order.</p>
+                <h3>Change of Mind:
+                </h3>
+                <p>If (for any reason) you change your mind and decide against continuing your project with us after placing your order, you can ask for refund within the first hour of placing your order. A 40% processing fee will be charged in other cases.
+                </p>
+                <h3>Incompetent Delivery:
+                </h3>
+                <p>Once the work is delivered, customers are only entitled to claim a refund once they have exhausted all the options detailed as under;
+                </p>
+                <p>If it does not comply with project requirements (as requested/documented by the customer). We are committed to provide our customers with 100% satisfaction and offer unlimited revisions to ensure that the delivery is up to the mark. We assign, re-assign and re-write your work to ensure complete satisfaction.
+                </p>
+                <p>Reserve FREE Pages for future, but of same value and you can avail them at any time.
+                </p>
+                <p>If we’re still not able to deliver what you asked for, refund will be processed with a mutual agreement on a set percentage (but only in the cases where the delivery is completely off the mark)
 
-                <h3>Contact Information</h3>
-                <p><?php echo WEBSITE_NAME; ?>. welcomes your comments regarding this Statement of
-                    Privacy. If you believe <?php echo WEBSITE_NAME; ?>. has not adhered to this Statement,
-                    please contact <?php echo WEBSITE_NAME; ?>. at <a
-                        href="mailto:<?php echo ADMIN_EMAIL; ?>">
-                        <?php echo ADMIN_EMAIL; ?>.</a>
-                    <?php echo WEBSITE_NAME; ?>. is located at <?php echo ADDRESS; ?>.&nbsp; We can be reached by phone at
-                    <a href="<?php echo PHONE_HREF; ?>"><?php echo PHONE; ?></a>.
+                </p>
+
+                <h3>Late Delivery:
+                </h3>
+                <p>We believe in “On Time Delivery” but if, for any reason we fail to deliver the asked service on time, after at least three attempts to contact us, your refund will be processed once it is established through documentary evidence that the late delivery was the company’s fault.
+                </p>
+
+                <h3>Refund Time Frame:
+                </h3>
+
+                <p>Refunds must be claimed within 120 days of delivery. Refunds claimed after the set time frame will not be entertained.
+                </p>
+                <p>All the customers must note the deadline for claiming refund at the time of placing order.
+                </p>
+
+                <h3>Cases where refund will not be issued:
+                </h3>
+
+                <p>In case of late delivery due to some minor technical errors, such as grammatical, typing, word count, missing references, etc., refunds will be processed with mutual agreement and the company will only settle with partial refunds or discounts reserved for future purchases.
+                </p>
+
+                <p>The company will not be responsible for any delay from the client’s end.
+                </p>
+
+                <p>Change of mind is not eligible for a refund.
+                </p>
+                <p>No refund will be issued on the basis of low writing.
+                </p>
+
+                <h3>Communication Policy
+                </h3>
+                <p>We at <?php echo WEBSITE_NAME; ?> value our customers privacy and we are highly committed to keeping client's personal information safe and secure. We collect only essential and inevitable information to process your order.
+
                 </p>
             </div>
         </div>
